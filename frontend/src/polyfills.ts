d8f0e9f00e0c11f3ec4af545db627a81372fb4b9
Adam Cardenas <cevaris@gmail.com>
2021-10-05 08:56:50 -0600

    fixed window global issue

--- frontend/src/polyfills.ts
+++ frontend/src/polyfills.ts
@@ -57,9 +57,10 @@ import './zone-flags';
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
-
+import 'zone.js/dist/zone'; // Included with Angular CLI.
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+// https://github.com/angular/angular-cli/issues/8160
+(window as any).global = window;
