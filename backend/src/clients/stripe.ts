31c201110e5fcf34d4c7a4e4995aac16ed3755f5
Adam Cardenas <cevaris@gmail.com>
2021-10-06 08:26:51 -0600

    typed stripe client

--- backend/src/clients/stripe.ts
+++ backend/src/clients/stripe.ts
@@ -1,1 +1,9 @@
-export const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+import Stripe from "stripe";
+
+if(!process.env.STRIPE_SECRET_KEY){
+  throw Error('missing STRIPE_SECRET_KEY env variable.');
+}
+
+export const stripeClient = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2020-08-27",
+});
--- backend/src/routes/munny/accounts.ts
+++ backend/src/routes/munny/accounts.ts
@@ -1,24 +1,27 @@
 import express from "express";
-import { stripe } from "../../clients/stripe";
+import Stripe from "stripe";
+import { stripeClient } from "../../clients/stripe";
 
 const router = express.Router();
 
+// curl -XPOST localhost:3000/munny/accounts/new.json
 router.post(
   "/munny/accounts/new.json",
   async (req: express.Request, res: express.Response) => {
-    let account;
+    let account: Stripe.Response<Stripe.Account>;
     try {
-      account = await stripe.accounts.create({
+      account = await stripeClient.accounts.create({
         type: "standard",
       });
       console.log(`created account: ${JSON.stringify(account)}`);
     } catch (error) {
       console.error("failed creating account", JSON.stringify(error));
+      return res.json({ success: false, error: error});
     }
 
-    let accountLink;
+    let accountLink: Stripe.Response<Stripe.AccountLink>;
     try {
-      accountLink = await stripe.accountLinks.create({
+      accountLink = await stripeClient.accountLinks.create({
         account: account.id,
         refresh_url: "http://localhost:3000/reauth",
         return_url: "http://localhost:3000/home",
@@ -28,6 +31,7 @@ router.post(
       console.log(`created account link: ${JSON.stringify(accountLink)}`);
     } catch (error) {
       console.error("failed creating account", JSON.stringify(error));
+      return res.json({ success: false, error: error});
     }
 
     res.json({ success: true, account_link: accountLink });
